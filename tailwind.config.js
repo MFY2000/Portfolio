a9fc912e4b88389ce72623356c9cb60f321aa884
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2023-07-15 01:32:49 +0500

    123

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,84 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        fira: ['Fira Code', 'sans-serif'],
       },
+      colors: {
+        'primary': '#000000',
+        'grey': '#ABB2BF',
+        'background': '#4A634D',
+        'white': '#FFFFFF',
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '2rem',
+        '4xl': '2.5rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+      },
+      boxShadow: {
+        'inner': 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+        'outline': '0 0 0 3px rgba(66, 153, 225, 0.5)',
+        'none': 'none',
+      },
+      borderRadius: {
+        'none': '0',
+        'sm': '0.125rem',
+        DEFAULT: '0.25rem',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'full': '9999px',
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '4': '4px',
+        '8': '8px',
+      },
+      spacing: {
+        '0': '0',
+        '1': '0.25rem',
+      },
+      width: {
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '2/4': '50%',
+        '3/4': '75%',
+      },
+      height: {
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '2/4': '50%',
+        '3/4': '75%',
+      },
+      minWidth: {
+        '0': '0',
+        'full': '100%',
+      },
+      minHeight: {
+        '0': '0',
+        'full': '100%',
+      },
+      maxWidth: {
+        'xs': '20rem',
+        'sm': '24rem',
+        'md': '28rem',
+        'lg': '32rem',
+        'xl': '36rem',
+      },
+      
+      }
     },
-  },
   plugins: [],
 }
