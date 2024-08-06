8044603ee8975216732c9c22d5be01251dbf983d
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2024-08-06 21:22:30 +0500

    Adding assets

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <div className='flex flex-row items-center justify-between '>
-        <div></div>
-        <div className='flex flex-row items-center justify-between' >
+      <div className='flex flex-grow-0 justify-between '>
+        <div><p>logo</p></div>
+        <div className='flex flex-row items-center justify-between ' >
           <a>#Home</a>
           <a>#Work</a>
           <a>#About-us</a>
